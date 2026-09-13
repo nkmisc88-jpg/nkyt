5bc0761ea80a49e27920a8d2cb46c8072721dd78
nkmisc88-jpg <nkmisc88@gmail.com>
2026-09-14 00:53:16 +0530

    Update generate_playlist.py

--- generate_playlist.py
+++ generate_playlist.py
@@ -51,12 +51,7 @@ def get_live_stream(url: str):
             "no_warnings": True,
             "skip_download": True,
             "format": "best[protocol^=m3u8]/best",
-            "extractor_args": {
-                "youtube": {
-                    "player_client": [client],
-                    "formats": ["missing_pot"],
-                }
-            },
+            "extractor_args": {"youtube": {"player_client": [client]}},
         }
         if has_cookies:
             ydl_opts["cookiefile"] = COOKIES_FILE
